0b84b8a6f6035d03cade0a639984f8543d1fc41f
Balaji Venkatraman <rslbalaji@gmail.com>
2026-06-13 21:37:03 +0530

    fix scrolling issue

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect, useRef } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { TopNav } from './TopNav'
 
 export function AppLayout() {
+  const mainRef = useRef<HTMLElement>(null)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    mainRef.current?.scrollTo({ top: 0, left: 0 })
+  }, [pathname])
+
   return (
     <div className="flex h-svh overflow-hidden bg-app-bg">
       <Sidebar />
       <div className="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
         <TopNav />
-        <main className="min-h-0 flex-1 overflow-y-auto p-gutter">
+        <main ref={mainRef} className="min-h-0 flex-1 overflow-y-auto p-gutter">
           <Outlet />
         </main>
       </div>
